97ffd356b555330f17121ac92908475f78a7aeb6
Nguyễn Ngọc Minh Huy <luyhuy19@gmail.com>
2018-03-13 11:15:35 +0700

    Log in

---
+++ App/Screen/LogIn.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { TextInput, View, Text, Button, Image } from 'react-native';
+
+export default class LogIn extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    login = () => {
+        alert('Log in successfully')
+    }
+    render() {
+        const {emailError, passwordError} = this.state;
+        return (
+            <View>
+                <View>
+                    <Image
+                        style={{ width: 200, height: 50 }}
+                        source={require('../../images/logo.jpg')}
+                    />
+                    <Text>
+                        LOG IN
+                    </Text>
+                </View>
+                <View>
+                    <View>
+                        <TextInput
+                            placeholder = "email"
+                            keyboardType= "email-address"
+                            onChangeText = {(email) => this.setState({email})}
+                        />
+                        {emailError && 
+                            <Text>{emailError}</Text>
+                        }
+                    </View>
+                    <View>
+                        <TextInput
+                            placeholder="password"
+                            secureTextEntry={true}
+                            onChangeText={(password) => this.setState({password})}
+                        />
+                        {passwordError &&
+                            <Text>{passwordError}</Text>
+                        }
+                    </View>
+                </View>
+                <View>
+                    <Button
+                        title = "Log in"
+                        onPress = {() => {this.login()}}
+                    />
+                    <Text>
+                        Forgot password?
+                    </Text>
+                    <Text>
+                        Create an account
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+}
